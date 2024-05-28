3c6d5a4c77f6857a2679d9bf903d5f723c2f2291
Эмир Ынтымак уулу <sheraliev270704@gmail.com>
2024-05-28 16:20:52 +0600

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 'use client';
 import Image from "next/image";
-import Pic1 from '../../public/images/1.jpg'
-import Pic2 from '../../public/images/2.jpeg'
+import Pic1 from '../../public/images/3.jpg'
+import Pic2 from '../../public/images/6.jpg'
 import { useScroll, useTransform, motion } from "framer-motion";
 import { useEffect, useRef } from "react";
 import Lenis from 'lenis';
